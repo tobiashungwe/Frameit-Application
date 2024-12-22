0344a412053dcff6f26e1ce327578b37935e0e35
tobiashungwe <tobias.hungwe@student.howest.be>
2024-12-22 18:11:31 +0100

    feat: add DocumentViewer component to display generated stories

---
+++ frontend_v2/src/components/DocumentViewer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+
+const DocumentViewer = ({ sanitizedContent }) => {
+  const lines = sanitizedContent.split("\n");
+
+  return (
+    <Box mt={4} p={2} bgcolor="background.paper" borderRadius={4}>
+      <Typography variant="h6">Generated Story</Typography>
+      <Box component="div" style={{ whiteSpace: "pre-wrap" }}>
+        {lines.map((line, index) => (
+          <Typography key={index} variant="body1">
+            {line}
+          </Typography>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default DocumentViewer;
