cd94e3cd0c2bd51c6d9974823497e8d57d86e777
Govaert Ward <ward.govaert2@student.howest.be>
2025-01-20 17:47:34 +0100

    creating class UserRepository

---
+++ backend/infrastructure/repositories/user_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from backend.domain.models.user_model import User
+
+class UserRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_user(self, username: str, hashed_password: str) -> User:
+        new_user = User(username=username, password=hashed_password)
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
+
+    def get_user_by_username(self, username: str) -> User:
+        return self.db.query(User).filter(User.username == username).first()
