5c07c43295bb2f48740dcafb9969cdb5fa6cb5c4
tobiashungwe <tobias.hungwe@student.howest.be>
2025-01-21 18:34:44 +0100

    Update settings to follow best practices, and change the port to an unused port

--- backend/core/database.py
+++ backend/core/database.py
@@ -14,9 +14,10 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 # 2. Build the database URL for mysql+pymysql
-#DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://frameit-admin:FrameIt123@mysql_container:3306/frameit")
-#DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-DATABASE_URL= "mysql+pymysql://frameit-admin:FrameIt123@mysql_container:3306/frameit"
+# DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://frameit-admin:FrameIt123@mysql_container:3306/frameit")
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL= "mysql+pymysql://frameit-admin:FrameIt123@mysql_container:3306/frameit"
+
 
 # 3. Create the SQLAlchemy engine
 #    - echo=True shows SQL logs in console, good for debugging.
