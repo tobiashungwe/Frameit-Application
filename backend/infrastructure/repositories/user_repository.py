039b43e35d8743acd2a3eee436c0708728368719
Govaert Ward <ward.govaert2@student.howest.be>
2025-01-21 21:46:14 +0100

    update user_repository

--- backend/infrastructure/repositories/user_repository.py
+++ backend/infrastructure/repositories/user_repository.py
@@ -2,9 +2,11 @@ from sqlalchemy.orm import Session
 from backend.domain.models.user_model import User
 
 class UserRepository:
+    @staticmethod
     def __init__(self, db: Session):
         self.db = db
 
+    @staticmethod
     def create_user(self, username: str, hashed_password: str) -> User:
         new_user = User(username=username, password=hashed_password)
         self.db.add(new_user)
@@ -12,5 +14,6 @@ class UserRepository:
         self.db.refresh(new_user)
         return new_user
 
+    @staticmethod
     def get_user_by_username(self, username: str) -> User:
         return self.db.query(User).filter(User.username == username).first()
