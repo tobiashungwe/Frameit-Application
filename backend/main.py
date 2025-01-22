58ee8ef3ba514dae3a165ac3d76c1d8936031c16
tobiashungwe <tobias.hungwe@student.howest.be>
2025-01-22 09:19:06 +0100

    temp fix let us try this

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ logfire.info("Starting FastAPI application...")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frameit.hungwevision.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
